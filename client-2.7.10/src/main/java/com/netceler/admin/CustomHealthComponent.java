ddf458cb9b2230defe64058ab13b46783122aae5
Julien Graglia <jgraglia@netceler.com>
2023-12-20 12:49:25 +0100

    demo

--- client-2.7.10/src/main/java/com/netceler/admin/CustomHealthComponent.java
+++ client-2.7.10/src/main/java/com/netceler/admin/CustomHealthComponent.java
@@ -10,6 +10,6 @@ import java.util.Set;
 public class CustomHealthComponent implements HealthIndicator {
     @Override
     public Health health() {
-        return Health.up().withDetail("foo", "bar").withDetail("tags", Set.of("A", "B")).build();
+        return Health.up().withDetail("foo10", "bar").withDetail("tags", Set.of("A10", "B10")).build();
     }
 }
--- client-2.7.11/src/main/java/com/netceler/admin/CustomHealthComponent.java
+++ client-2.7.11/src/main/java/com/netceler/admin/CustomHealthComponent.java
@@ -10,6 +10,6 @@ import java.util.Set;
 public class CustomHealthComponent implements HealthIndicator {
     @Override
     public Health health() {
-        return Health.up().withDetail("foo", "bar").withDetail("tags", Set.of("A", "B")).build();
+        return Health.up().withDetail("foo11", "bar").withDetail("tags", Set.of("A11", "B11")).build();
     }
 }
